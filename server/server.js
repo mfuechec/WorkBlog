0e719c49fc3f94ee8bc338673606959ea360aba3
Mark Fuechec <fuechec.mark@gmail.com>
2020-06-25 01:20:41 -0500

    Updated port number

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
 app.listen(port, function() {
-    console.log('listening')
+    console.log(`Listening on port ${port}`)
 });
